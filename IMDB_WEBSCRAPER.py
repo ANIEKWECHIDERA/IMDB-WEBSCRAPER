2bc78e91a4b4105a550affc95e354cdeddb21539
ANIEKWECHIDERA <128926705+ANIEKWECHIDERA@users.noreply.github.com>
2023-04-07 09:34:27 +0100

    Update IMDB_WEBSCRAPER.py

--- IMDB_WEBSCRAPER.py
+++ IMDB_WEBSCRAPER.py
@@ -1,24 +1,29 @@
+#first, i imported the necessary libraries for the project 
+
 from bs4 import BeautifulSoup
 import requests
 import openpyxl
 
-#creating an excel workbook and sheet
+#creating an excel workbook to save the info into
 excel = openpyxl.Workbook()
 sheet = excel.active
 sheet.title = 'IMDB top rated movies data'
 sheet.append(['Movie rank', 'Movie name', 'Movie year', 'Movie rating'])
 
-#extracting data
+#extracting the data from the site by parsing the HTML content of the site using beatifulsoup
+and the whole code is basically done in a 'try' block incase of errors
 url = "https://www.imdb.com/chart/top/"
 try:
     source = requests.get(url)
     source.raise_for_status()
-    
     soup = BeautifulSoup(source.text, 'html.parser')
     #print(soup)
+    #searching the parsed HTML content for the location of the info i want to scrape
     
     movies = soup.find('tbody', class_= "lister-list").find_all('tr')
     #print(len(movies))
+    
+    #creating a loop to search the in for from the apropriate tag
     
     for movie in movies:
         
@@ -31,14 +36,15 @@ try:
         rating = movie.find('td', class_="ratingColumn imdbRating").strong.text
         
         print(rank, name, year, rating)
-        #break
-        sheet.append([rank, name, year, rating])
+       #now i have to append the result of the search to the excel file
+        
+        sheet.append([int(rank), name, int(year), int(float(rating))])
     
 except Exception as e:
    print(e)
 
-#saving the excel file
-excel.save(r'C:\Users\chide\Desktop\imdb movie rating2.xlsx')
+#this is saving the excel file to my desktop withthe name 'imdb movie rating.xlsx'
+excel.save(r'C:\Users\chide\Desktop\imdb movie rating.xlsx')
 
 
 
